4847743a64e68c63506c2e8cc5e18163c763e571
Munawar <munawar.hassan@in.ibm.com>
2022-03-17 21:37:06 +0530

    apigateway updated

--- src/main/java/com/ibm/demo/shoppingcartapigateway/ShoppingCartApiGatewayController.java
+++ src/main/java/com/ibm/demo/shoppingcartapigateway/ShoppingCartApiGatewayController.java
@@ -1,11 +1,17 @@
 package com.ibm.demo.shoppingcartapigateway;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 
 @RestController
-@RequestMapping("/currencycloudapigateway")
+@RequestMapping("/shoppingcartapigateway")
 public class ShoppingCartApiGatewayController {
 	
 	@RequestMapping(path = "/default", method = RequestMethod.GET)
@@ -15,5 +21,19 @@ public class ShoppingCartApiGatewayController {
 		
 		return "Welcome To CurrencyCloudApiGateway";
 	}
+	
+	
+	/*
+    	@GetMapping("/greeting")
+	    public String greeting(@AuthenticationPrincipal OidcUser oidcUser, Model model,
+	                           @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client) {
+    		  model.addAttribute("username", oidcUser.getEmail());
+    	        model.addAttribute("idToken", oidcUser.getIdToken());
+    	        model.addAttribute("accessToken", client.getAccessToken());
+
+    	        return "greeting";
+	    }
+	*/
+
 
 }
